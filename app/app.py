485ededcd532a7d6759560d2cb927f2af67ba736
Daniel <danielvidal130602@gmail.com>
2024-05-18 14:58:54 +0200

    app

--- app/app.py
+++ app/app.py
@@ -2,23 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
-status = "Stopped"
 
 @app.route('/')
 def index():
-    return render_template('index.html', status=status)
+    return render_template('home.html')
 
-@app.route('/play', methods=['POST'])
-def play():
-    global status
-    status = "Playing..."
-    return redirect(url_for('index'))
-
-@app.route('/stop', methods=['POST'])
-def stop():
-    global status
-    status = "Stopped"
-    return redirect(url_for('index'))
+@app.route('/playing')
+def playing():
+    return render_template('playing.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
